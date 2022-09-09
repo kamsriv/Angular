646b454c897664a5a3e3628cb5f1d2eb4790fa07
kamsriv <kamineni_srinivasarao@yahoo.co.in>
2022-09-09 20:54:55 +0530

    Button got added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AutoSuggestionComponent } from './auto-suggestion/auto-suggestion.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, MatButtonModule],
   exports: [],
   declarations: [AppComponent, HelloComponent, AutoSuggestionComponent],
   bootstrap: [AppComponent],
